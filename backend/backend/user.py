550c89568013653d8e39636a2af9f1020cfc3dfd
opxzy22 <aopeyem84@gmail.com>
2024-04-05 07:09:46 +0100

    manage User model

--- backend/backend/user.py
+++ backend/backend/user.py
@@ -9,17 +9,23 @@ def generate_uuid() -> str:
     return str(uuid.uuid4())
 
 class DB_user:
-
-    def create_user(self, email, firstName, lastName, password) -> User:
+    """ 
+    """
+    def create_user(self, email, firstName, lastName,
+                     password) -> User:
+        """ A method to create new user
+        """
         hashed_password = make_password(password)
         try:
             user = User.objects.create(email=email, firstname=firstName,
-                    lastname=lastName, password=hashed_password)
+                                        lastname=lastName, password=hashed_password)
             return user
         except Exception:
             print('user already exist')
     
     def find_user(self, **kwargs) -> User:
+       """  a methed to find user with a key word arg
+       """
        user = User.objects.filter(**kwargs).first()
        
        if not user:
@@ -28,8 +34,10 @@ class DB_user:
        return user
     
     def valid_login(self, email: str, password: str) -> bool:
+        """ a method to verify users login details
+        """
+        user = self.find_user(email=email)
         try:
-            user = self.find_user(email=email)
             if user:
                 if check_password(password.encode('utf-8'),
                                   user.password):
@@ -42,6 +50,8 @@ class DB_user:
             return False
     
     def update_user(self, user_id: int, **kwargs):
+        """ a method to update users details in the db
+        """
         user = self.find_user(id=user_id)
 
         if user:
@@ -61,16 +71,25 @@ class DB_user:
             raise ValueError('No user found with the provided user_id')
         
     def create_session(self, email):
+        """ a method to create a session id
+            updates the session_id in db
+        """
         user = self.find_user(email=email)
 
         try:
             if user:
+                print(user)
                 session_id = generate_uuid()
                 self.update_user(user.id, session_id=session_id)
-        except Exception:
-            raise 
 
-    def get_user_by_session_id(self, session_id) -> Union[User, None]:
+                return session_id
+        except Exception:
+            raise
+
+    def get_user_by_session_id(self,
+                            session_id) -> Union[User, None]:
+        """ a method to get user by session_id
+        """
         if session_id is None:
             return None
         try:
@@ -80,6 +99,9 @@ class DB_user:
             return None
         
     def destroy_session(self, user_id: int)-> None:
+        """ a method to delete session_id
+            updates the session_id to None in db
+        """
         user = self.find_user(id=user_id)
         try:
             if user:
@@ -89,6 +111,9 @@ class DB_user:
             return None
         
     def get_reset_token(self, email):
+        """ a method to get a reset_token
+            updates it to the db
+        """
         user = self.find_user(email=email)
         try:
             if user:
@@ -98,6 +123,10 @@ class DB_user:
             raise
 
     def reset_password(self, reset_token, password):
+        """ a method to reset password 
+            gets user by reset token
+            update new password in db
+        """
         user = self.find_user(reset_token=reset_token)
         try:
             if user:
