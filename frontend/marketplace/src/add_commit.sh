89db100a5060e3776389071c1d0c99e09aeb3db4
opxzy22 <aopeyem84@gmail.com>
2024-04-05 08:10:28 +0100

    react app for frontend

---
+++ frontend/marketplace/src/add_commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git add app.js
+git commit -m "entry point for my react app"
+
+git add Homepage.js
+git commit -m "display product with count"
+
+git CreateAd.js
+git commit -m "create new advert"
+
+git add productDetails.js
+git commit -m "view details for each product"
+
+git add ProductList.js
+git commit -m "display trendings ads"
+
+git add SearchComponent.js
+git commit -m "search for product"
+
+git add SearchResult.js
+git commit -m "display search results"
+
+git add SinginPage.js
+git commit -m "Implement sign in page"
