e3df8c2eee54bce4a0bd4924d7ea3c0fdf76d001
opxzy22 <aopeyem84@gmail.com>
2024-05-15 12:39:02 +0100

    updated the models

---
+++ backend/backend/add_commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git add admin.py
+git commit -m "admin configuration"
+
+git add categorymanager.py
+git commit -m "manage Category model features"
+
+git add models.py
+git commit -m "project models"
+
+git add productmanager.py
+git commit -m "manage Product model features"
+
+git add settings.py
+git commit -m "django settings"
+
+git add urls.py
+git commit -m "define urls path"
+
+git add user.py
+git commit -m "manage User model"
+
+git add views.py
+git commit -m "Implement api's endpoint"
